25ed3d2919cd1979c0918b94435b6f691b232cb2
carlosaurelio94 <106714958+carlosaurelio94@users.noreply.github.com>
2022-06-21 23:19:49 -0300

    Add files via upload

---
+++ areasFunciones.py
@@ -0,0 +1,12 @@
+def areaTriangulo(base, altura):
+    return base*altura/2
+
+resultado = areaTriangulo(10, 5)
+print('El area de un triangulo es: ', resultado, 'metros cuadrados')
+
+def areaCirculo(radio):
+    pi = 3.14
+    return pi*radio**2
+
+resultado =areaCirculo(5)
+print('El area del circulo es: ', resultado, 'metros cuadrados')
